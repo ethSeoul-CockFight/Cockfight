dc66887b561f881bc41f36030eb870e552a9cf1c
arypte <wjs505@gmail.com>
2024-03-31 02:29:38 +0900

    update: Market

---
+++ front/src/components/ChikenBox.jsx
@@ -0,0 +1,37 @@
+import { useContext, useEffect } from "react";
+import { formatAmount } from "../utils/helpper";
+import { buyUser } from "../evmInteraction/buyChickens";
+import { AppContext } from "../App";
+
+const ChikenBox = ({ tokenId, price }) => {
+  const { account, vault_c, decimals } = useContext(AppContext);
+
+  const onClickUserBuyChicken = async () => {
+    await buyUser(account, vault_c, tokenId, price);
+  };
+  return (
+    <div className="flex items-center justify-between p-4 shadow-md rounded-lg">
+      <div className="flex items-center justify-center">
+        {/* 상품 이미지와 정보를 가운데 정렬 */}
+        <img
+          src={`${process.env.PUBLIC_URL}/images/c_classic.png`}
+          alt="Volatile Chicken"
+          className="rounded-full h-16 w-16"
+        />
+        <div className="flex flex-col items-center ml-2">
+          <h3 className="text-lg font-bold">Stable Chicken</h3>
+          <p className="text-sm text-gray-600">Condition : 70%</p>
+        </div>
+        <div className="ml-3">{formatAmount(price, 14)} USDC</div>
+      </div>
+      <button
+        className="bg-blue-500 text-white px-4 py-2 rounded-md"
+        onClick={onClickUserBuyChicken}
+      >
+        구매
+      </button>
+    </div>
+  );
+};
+
+export default ChikenBox;
--- front/src/pages/Market.jsx
+++ front/src/pages/Market.jsx
@@ -1,10 +1,55 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext, useEffect, useState } from "react";
+
+import ChikenBox from "../components/ChikenBox";
+import { getSellingList } from "../evmInteraction/connect";
+import LoadingPage from "../components/Loading";
+import { AppContext } from "../App";
 
 const Market = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const { vault_c, account } = useContext(AppContext);
+
+  const [sellList, setSellList] = useState();
+
+  const getSellingData = async () => {
+    const response = await getSellingList(vault_c);
+    setSellList(response);
+    console.log(response);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getSellingData();
+  }, []);
+
   return (
-    <div className="h-full mb-24 overflow-y-scroll overflow-x-hidden scrollbar-hide"></div>
+    <>
+      {isLoading ? (
+        <div className="min-h-screen flex justify-center text-3xl font-bold pt-80">
+          <LoadingPage />
+        </div>
+      ) : (
+        <div className="h-full overflow-y-scroll overflow-x-hidden scrollbar-hide">
+          {/* 헤더 */}
+          <div
+            className="flex justify-around h-12 items-center  border-b-4
+     border-zinc-200"
+          >
+            <div className="text-2xl font-bold">Market</div>
+          </div>
+
+          <div>
+            {sellList[0]?.map((v, i) => (
+              <ChikenBox key={i} tokenId={v} price={sellList[1][i]} />
+            ))}
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
 export default Market;
+
+// 판매 리스트, getsellingchikens(tokenID, price)
+// 구매하기, - buychkien
